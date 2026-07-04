9622a4abb786053d19c68aa15f9438ecd4e3b0e1
jathavan mahendrarajah <physickness@gmail.com>
2026-07-04 09:24:48 +0530

    space time ray tracig

--- ray.h
+++ ray.h
@@ -8,12 +8,16 @@ P_t = t*B +A;
 class ray{
 public:
     ray(){};
-    ray(const glm::vec3 &a, const glm::vec3 &b) : A(a) , B(b){}
-    glm::vec3 origin() const{return A;}
-    glm::vec3 direction() const {return B;}
-    glm::vec3 point_at_param(float t )const{return A+t*B;}
-    glm::vec3 A;
-    glm::vec3 B;
+    ray(const glm::vec3 &a, const glm::vec3 &b, double time) : orig(a) , dir(b), tm(time){}
+    ray(const glm::vec3 &a, const glm::vec3 &b) : ray(a,b,0){}
+    glm::vec3 origin() const{return orig;}
+    glm::vec3 direction() const {return dir;}
+    glm::vec3 point_at_param(float t )const{return orig+t*dir;}
+    double time() const{return tm;}
+
+    glm::vec3 orig;
+    glm::vec3 dir;
+    double tm;
 };
 
 #endif
